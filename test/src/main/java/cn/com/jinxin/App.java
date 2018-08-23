1b4b14941bf5c774d1c833991d4e49d088837dfd
周锦鑫 <zhoujinxin@yushutec.com>
2018-08-23 17:00:45 +0800

    2 commit

---
+++ test/src/main/java/cn/com/jinxin/App.java
@@ -0,0 +1,12 @@
+package cn.com.jinxin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(App.class).run(args);
+	}
+}
---
+++ test/src/main/java/cn/com/jinxin/TestController.java
@@ -0,0 +1,32 @@
+package cn.com.jinxin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
+
+@RestController(value="/http")
+public class TestController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<String> getTest(String id) {
+		if(id==null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public Person getPerson(@RequestBody Person person) {
+		return person;
+	}
+	
+	
+}
